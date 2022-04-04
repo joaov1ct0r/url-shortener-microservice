02fe3fa0893e9e6b59e17d6a0c02a43cfaef4809
Joao Victor <jov9051@outlook.com>
2022-04-04 19:06:38 -0300

    feat: handleNewUrl criado em userController.js

---
+++ src/controllers/userController.js
@@ -0,0 +1,20 @@
+const handleNewUrl = (req, res) => {
+    let { url } = req.body;
+
+    if (!isUrl(url)) {
+        return res.json({
+            error: 'invalid url'
+        });
+    } else {
+        id++;
+
+        const link = {
+            original_url: url,
+            short_url: `${id}`
+        };
+
+        links.push(link);
+
+        return res.json(link);
+    }
+};
