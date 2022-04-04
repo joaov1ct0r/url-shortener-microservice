0ae7b4fdfe15df4291b051b53ac13f47eeec8570
Joao Victor <jov9051@outlook.com>
2022-04-04 19:21:28 -0300

    feat: userRouter sendo utilizado em todas rotas /api

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import express from 'express';
 
 import cors from 'cors';
 
-import userRoutes from './routes/userRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.json());
+app.use('/api', express.json(), userRouter);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log('Server running');
