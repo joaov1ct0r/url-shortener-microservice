e8f97113a456e017d22b2c5ca798b38c2a13caa1
Joao Victor <jov9051@outlook.com>
2022-04-04 19:14:32 -0300

    feat: handleShortUrl sendo utilizado na rota /shorturl/:url

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,3 +3,5 @@ import express from 'express';
 import { handleNewUrl, handleShortUrl } from '../controllers/userController.js';
 
 const userRouter = express.Router();
+
+userRouter.get('/shorturl/:url', handleShortUrl);
